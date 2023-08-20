4a078ccb33c5e4cda8e992a139dfdc9d2442e302
Sanket Jain <sanketjain@Sankets-MacBook-Pro.local>
2023-08-20 19:11:44 +0530

    feat: added javadoc and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# messager-system
+# messanger-system
+
+
+## Run Spring Boot application
+```
+mvn clean install
+mvn spring-boot:run
+```
+
+## Run following SQL insert statements
+```
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+```
+
+## Postman screenshots
+
+![](Screenshot 2023-08-20 at 19.02.12.png)
+![](Screenshot 2023-08-20 at 19.02.22.png)
+![](Screenshot 2023-08-20 at 19.02.25.png)
+![](Screenshot 2023-08-20 at 19.02.27.png)
+![](Screenshot 2023-08-20 at 19.02.29.png)
+![](Screenshot 2023-08-20 at 19.02.32.png)
+
+## CURLS for the same
+
+### /send/text/user
+```
+curl --location --request POST 'localhost:8080/api/auth/create/user' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "sanky2",
+    "password": "test@123",
+    "role": [
+        "ROLE_USER",
+        "ROLE_ADMIN"
+    ]
+}'
+```
+
+###/login
+```aidl
+curl --location --request POST 'localhost:8080/api/auth/login' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "sanky2",
+    "password": "test@123"
+}'
+```
+
+###/get/user
+```aidl
+curl --location --request GET 'localhost:8080/api/messenger/get/users' \
+--header 'Authorization: Bearer  eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW5reTIiLCJpYXQiOjE2OTI1MjgwMTEsImV4cCI6MTY5MjYxNDQxMX0.6GeQq_Php48Skts8jk3h8wng7fTwfngC4I4tdsTjm_U'
+```
+
+###/get/history/{chatId}
+```aidl
+curl --location --request GET 'localhost:8080/api/messenger/get/history/1/sanky1' \
+--header 'Authorization: Bearer  eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW5reTIiLCJpYXQiOjE2OTI1MjgwMTEsImV4cCI6MTY5MjYxNDQxMX0.6GeQq_Php48Skts8jk3h8wng7fTwfngC4I4tdsTjm_U'
+```
+
+###/get/unread/{userName}
+```aidl
+curl --location --request GET 'localhost:8080/api/messenger/get/unread/sanky1' \
+--header 'Authorization: Bearer  eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW5reTIiLCJpYXQiOjE2OTI1MjgwMTEsImV4cCI6MTY5MjYxNDQxMX0.6GeQq_Php48Skts8jk3h8wng7fTwfngC4I4tdsTjm_U'
+```
+
+###/send/text/user
+```aidl
+curl --location --request POST 'localhost:8080/api/messenger/send/text/user' \
+--header 'Authorization: Bearer  eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW5reTIiLCJpYXQiOjE2OTI1MjgwMTEsImV4cCI6MTY5MjYxNDQxMX0.6GeQq_Php48Skts8jk3h8wng7fTwfngC4I4tdsTjm_U' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "fromUserName": "sanky",
+  "toUserName": "sanky1",
+  "chatId": 1,
+  "text": "Four message"
+}'
+```
+
--- src/main/java/com/example/messanger/service/MessengerServiceImpl.java
+++ src/main/java/com/example/messanger/service/MessengerServiceImpl.java
@@ -35,11 +35,18 @@ public class MessengerServiceImpl implements MessengerService {
     @Autowired
     private ChatReadByRepository chatReadByRepository;
 
+    /**
+     * gets all UserNames
+     *
+     * @return
+     */
     @Override
     public MessageResponse getUsers() {
 
+        //finds all users
         List<User> userList = userRepository.findAll();
 
+        //filtering only userName
         List<String> userName = new ArrayList<>();
         if (!CollectionUtils.isEmpty(userList)) {
             userName = userList.stream().map(p -> p.getUsername()).collect(Collectors.toList());
@@ -48,16 +55,27 @@ public class MessengerServiceImpl implements MessengerService {
         return new MessageResponse(EStatus.SUCCESS, userName);
     }
 
+    /**
+     * gets Users unread messages for a given chat
+     *
+     * @param userName
+     * @return
+     * @throws Exception
+     */
     @Override
     public MessageResponse getUnreadMessages(String userName) throws Exception {
 
-
+        //get User's data
         User user = userRepository.findByUsername(userName).orElseThrow(() -> new Exception("User not found with userName " + userName));
+
+        //gets user's unread messages from chatReadBy table
         List<ChatReadBy> chatReadBIES = chatReadByRepository.getChatByUserAndStatus(user.getId(), false);
+
+        //Map of chatToMessages -> telling user unread messages
         Map<Long, List<String>> chatToMessages = new HashMap<>();
         if (!CollectionUtils.isEmpty(chatReadBIES)) {
-            for (ChatReadBy chatReadBy : chatReadBIES) {
 
+            for (ChatReadBy chatReadBy : chatReadBIES) {
                 Message message = chatReadBy.getMessage();
                 Long chatId = message.getChat().getId();
                 if (chatToMessages.containsKey(chatId)) {
@@ -67,39 +85,51 @@ public class MessengerServiceImpl implements MessengerService {
                 } else {
                     chatToMessages.put(chatId, Arrays.asList(message.getText()));
                 }
-
-
             }
         }
+        //returning based on unreadMessages
         if (chatToMessages.size() > 0) {
             return new MessageResponse(EStatus.SUCCESS, "You have message(s)", chatToMessages);
         }
         return new MessageResponse(EStatus.SUCCESS, "No new Messages");
     }
 
+    /**
+     * sends message based on chatId
+     *
+     * @param sendTextUserRequest
+     * @return
+     * @throws Exception
+     */
     @Override
     public MessageResponse sendMessage(SendTextUserRequest sendTextUserRequest) throws Exception {
 
-
+        //fetches user's
         User fromUser = userRepository.findByUsername(sendTextUserRequest.getFromUserName()).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + sendTextUserRequest.getFromUserName()));
         User toUser = userRepository.findByUsername(sendTextUserRequest.getToUserName()).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + sendTextUserRequest.getToUserName()));
 
+        //if chatId is null considering new chat
         if (Objects.nonNull(sendTextUserRequest) && sendTextUserRequest.getChatId() == null) {
 
+            //adding chat entry
             Chat chatEntry = Chat.builder().
                     lastSeenMessage(sendTextUserRequest.getText()).
                     lastSeenUser(fromUser).type(ChatType.ONE_ONE).
                     build();
             chatEntry = chatRepository.save(chatEntry);
 
+            //TODO:: can move to common place
+            //storing message with chatId
             Message message = Message.builder().
                     chat(chatEntry).user(fromUser).text(sendTextUserRequest.getText()).build();
             messageRepository.save(message);
 
+            //storing chatread status as false for toUser
             ChatReadBy chatReadBy = ChatReadBy.builder().
                     message(message).user(toUser).isRead(false).build();
             chatReadByRepository.save(chatReadBy);
 
+            //creating a entry of chatToUser for loading inbox
             ChatParticipants fromChatParticipant = ChatParticipants.builder().
                     chat(chatEntry).user(fromUser).
                     build();
@@ -112,9 +142,12 @@ public class MessengerServiceImpl implements MessengerService {
             chatParticipantsRepository.save(toChatParticipant);
 
         } else {
+            //same flow as above for exisitng chat
             Chat chatEntry = chatRepository.findById(sendTextUserRequest.getChatId()).orElseThrow(() -> new Exception("Chat Not Found with chatId: " + sendTextUserRequest.getChatId()));
             chatEntry.setLastSeenMessage(sendTextUserRequest.getText());
             chatEntry.setLastSeenUser(fromUser);
+
+            //TODO:: can move to common place
             Message message = Message.builder().
                     chat(chatEntry).user(fromUser).text(sendTextUserRequest.getText()).build();
             messageRepository.save(message);
@@ -126,11 +159,22 @@ public class MessengerServiceImpl implements MessengerService {
         return new MessageResponse(EStatus.SUCCESS, "");
     }
 
+    /**
+     * gets History based on chatId and user
+     *
+     * @param chatId
+     * @param fromUserName
+     * @return
+     * @throws Exception
+     */
     @Override
     public MessageResponse getHistory(Long chatId, String fromUserName) throws Exception {
 
+        //gets fromUser
         User user = userRepository.findByUsername(fromUserName).orElseThrow(() -> new Exception("fromUserNotFound"));
+        //finds all the messages for user based on a given chat
         List<Message> messages = messageRepository.findByChatIdOrderByCreatedAtDesc(chatId);
+        // traversing through messages for desired response and marking messages a read
         List<ChatHistoryResponse> chatHistoryResponses = new ArrayList<>();
         List<Long> messageId = new ArrayList<>();
         if (!CollectionUtils.isEmpty(messages)) {
@@ -139,10 +183,11 @@ public class MessengerServiceImpl implements MessengerService {
                 chatHistoryResponses.add(new ChatHistoryResponse(message.getUser().getUsername(), message.getText()));
             }
         }
+        //marking messages a read
         if (!CollectionUtils.isEmpty(messageId)) {
             List<ChatReadBy> chatReadBIES = chatReadByRepository.getChatReadByMessageAndUser(messageId, user.getId());
             if (!CollectionUtils.isEmpty(chatReadBIES)) {
-                for(ChatReadBy chatReadBy:chatReadBIES) {
+                for (ChatReadBy chatReadBy : chatReadBIES) {
                     chatReadBy.setIsRead(true);
                     chatReadByRepository.save(chatReadBy);
                 }
